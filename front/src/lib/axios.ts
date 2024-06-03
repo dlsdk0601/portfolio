4cf5f514808bb68c3e4819d5e948b92a613036ff
dlsdk0601 <dlsdk0601@gmail.com>
2024-06-04 02:08:29 +0900

    feat: axios 모듈 생성

---
+++ front/src/lib/axios.ts
@@ -0,0 +1,137 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { sleep } from "sleepjs";
+import { isBlock } from "../store/isBlock";
+import { config } from "../config/config";
+
+export const axiosInstance = axios.create({
+  baseURL: "/api",
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  async (axiosConfig) => {
+    // login 기반의 웹이 아니라서 token 처리는 하지 않는다.
+    return axiosConfig;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  async (res) => {
+    return res;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
+
+export class ApiBase {
+  private counter = 0;
+
+  with = async <T>(block: () => Promise<T>) => {
+    isBlock.getState().up();
+    try {
+      return block();
+    } finally {
+      isBlock.getState().down();
+    }
+  };
+
+  errHandle = (err: unknown) => {
+    // TODO :: 에러 메세지 처리를 어떻게 할지 결정하면 추가
+    if (axios.isAxiosError(err)) {
+      return err.response?.data.message ?? err.message;
+    }
+
+    return "서버 통신이 원활하지 않습니다./n잠시 후 다시 이용해주세요.";
+  };
+
+  get = async <T>(
+    endpoint: string,
+    query?: any,
+    axiosRequestConfig?: AxiosRequestConfig<any>,
+  ): Promise<T | null> => {
+    return this.with(async () => {
+      try {
+        if (config.apiDelay) {
+          await sleep(config.apiDelay);
+        }
+
+        const res = await axiosInstance.get(endpoint, { ...axiosRequestConfig, params: query });
+        return res.data;
+      } catch (e) {
+        this.errHandle(e);
+        return null;
+      }
+    });
+  };
+
+  post = async <T>(
+    endpoint: string,
+    data?: any,
+    axiosRequestConfig?: AxiosRequestConfig<any>,
+  ): Promise<T | null> => {
+    return this.with(async () => {
+      try {
+        if (config.apiDelay) {
+          await sleep(config.apiDelay);
+        }
+
+        const res = await axiosInstance.post(endpoint, data, axiosRequestConfig);
+        return res.data;
+      } catch (e) {
+        this.errHandle(e);
+        return null;
+      }
+    });
+  };
+
+  put = async <T>(
+    url: string,
+    data: Record<string, string>,
+    axiosRequestConfig?: AxiosRequestConfig<any>,
+  ): Promise<T | null> => {
+    return this.with(async () => {
+      try {
+        if (config.apiDelay) {
+          await sleep(config.apiDelay);
+        }
+
+        const res = await axiosInstance.put(url, data, axiosRequestConfig);
+
+        return res.data;
+      } catch (e) {
+        this.errHandle(e);
+        return null;
+      }
+    });
+  };
+
+  delete = async <T>(
+    url: string,
+    data: Record<string, string>,
+    axiosRequestConfig?: AxiosRequestConfig<any>,
+  ): Promise<T | null> => {
+    return this.with(async () => {
+      try {
+        if (config.apiDelay) {
+          await sleep(config.apiDelay);
+        }
+
+        let pureUrl = url;
+        for (const [key, value] of Object.entries(data)) {
+          pureUrl = pureUrl.replace(`:${key}`, value);
+        }
+
+        const res = await axiosInstance.delete(pureUrl, axiosRequestConfig);
+
+        return res.data;
+      } catch (e) {
+        this.errHandle(e);
+        return null;
+      }
+    });
+  };
+}
