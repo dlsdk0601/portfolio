a01a3326c01c99a14c412ecb4dbcc0c96a3db07b
dlsdk0601 <dlsdk0601@gmail.com>
2024-06-08 19:59:32 +0900

    feat: server init

---
+++ server/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='portfolio_was',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # was
+        'fastapi==0.111.0',
+        'werkzeug==3.0.3',
+        'sconfig==0.0.3',
+        'pydantic==2.7.3',
+        'stringcase==1.2.0',
+        # DB
+        'SQLAlchemy==2.0.30',
+        'FastAPI-SQLAlchemy==0.2.1',
+        'alembic==1.13.1',
+        'psycopg2-binary==2.9.9',
+        'coint-paginatify-sqlalchemy==0.0.4',
+        'Faker==25.8.0',
+        # ex
+        'pytz==2024.1',
+        'types-pytz==2024.1.0.20240417',
+        'more-itertools==10.2.0',
+        # 정적 타입 분석
+        'mypy==1.10.0',
+        'watchdog==4.0.1',
+        'boto3==1.34.122',
+        'requests==2.32.3',
+        'types-requests==2.32.0.20240602',
+    ]
+)
---
+++ server/was/config.py
@@ -0,0 +1,38 @@
+from os import environ
+
+from sconfig import configure
+
+PORT = 8000
+DEBUG = False
+SECRET_KEY = '30d3ce2d112b78ca50ad16ca4448d9188856a912b57a677efb9e349b8c863263'
+SECRET_PASSWORD_BASE_SALT = SECRET_KEY
+
+# 파일 업로드 제한
+FILE_UPLOAD_MAX_SIZE = 100 * 1024 * 1024
+
+# DB -> 한글 정렬을 위한 DB 설정
+DB_COLLLNAME = 'und-x-icu'
+
+# SQLAlchemy
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = environ.get(
+    'DATA_BASE_URL',
+    f'postgres://postgres@{environ.get("DOCKER_HOST", "localhost")}:30322/portfolio'
+)
+SQLALCHEMY_ECHO = False
+
+# AWS
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_REGION = 'ap-northeast-2'
+AWS_BUCKET = ''
+
+
+configure(__name__)
+
+# SALT 는 Bytes 타입이어야 한다.
+SECRET_PASSWORD_BASE_SALT = bytes.fromhex(SECRET_PASSWORD_BASE_SALT)
+
+if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + SQLALCHEMY_DATABASE_URI.removeprefix('postgres://')
+
