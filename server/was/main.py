14b81f5dedc2f299420b915c1ff404ebc1b35e48
dlsdk0601 <dlsdk0601@gmail.com>
2024-06-14 00:34:46 +0900

    feat: app 을 import 해와야 하기 때문에 app.docs_url 을 프린트 해준다

--- server/was/main.py
+++ server/was/main.py
@@ -4,4 +4,5 @@ from was import config
 from was.application import app
 
 if __name__ == '__main__':
+    print(app.docs_url)
     uvicorn.run('main:app', port=config.PORT, host='0.0.0.0', reload=True)
