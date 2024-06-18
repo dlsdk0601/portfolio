21e95318b2e92a41d954d2950ba722ee1b6bfd21
dlsdk0601 <dlsdk0601@gmail.com>
2024-06-19 01:05:29 +0900

    feat: layoutSelector 로직 생성

--- admin/src/view/layout/layoutSelector.tsx
+++ admin/src/view/layout/layoutSelector.tsx
@@ -2,20 +2,47 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { managerModel } from "../../store/managerModel";
+import { Urls } from "../../url/url.g";
 
-function LayoutSelector() {
+export function LayoutSelector() {
   const path = usePathname();
 
   // 예외 URL
   const isSpecialUrl = ["/_"].some((prefix) => path.startsWith(prefix));
 
   if (isSpecialUrl) {
+    // OPT :: 예외 URL 이 추가 된다면 Replace 시킬 것.
     return <></>;
   }
 
-  // 로그인
-  if (path.startsWith("/sign/")) {
+  if (path === Urls["sign-in"].page.url()) {
     return <></>;
+  }
+
+  return <AdminApp />;
+}
+
+export function AdminApp() {
+  const manager = managerModel((state) => state);
+  const path = usePathname();
+
+  useEffect(() => {
+    if (manager.isSigned) {
+      return;
+    }
+
+    manager.init();
+  }, []);
+
+  // 계정 정보 초기화 중
+  if (!manager.initialized) {
+    return <></>;
+  }
+
+  if (!manager.isSigned) {
+    // 로그인 전 이라면 로그인 페이지로
+    return <Replace url={Urls["sign-in"].page.url({ returnTo: path })} />;
   }
 
   return <></>;
