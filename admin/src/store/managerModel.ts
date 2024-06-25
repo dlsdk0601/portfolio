36357ea09f109de3b17a031b93fc2527757ec67e
dlsdk0601 <dlsdk0601@gmail.com>
2024-06-26 00:46:46 +0900

    feat: deInit 추가, init API 연동

--- admin/src/store/managerModel.ts
+++ admin/src/store/managerModel.ts
@@ -1,38 +1,72 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
+import { isNil } from "lodash";
 import { config } from "../config/config";
+import { api } from "../api/api";
 
 interface ManagerModelState {
   initialized: boolean;
+  pk: number | null;
   id: string;
+  name: string;
   email: string;
   phone: string;
+  job: string;
   token: string | null;
   refreshToken: string | null;
 }
 
 interface ManagerModelAction {
+  init: () => Promise<void>;
   setToken: (token: string, refreshToken: string) => void;
-  setId: (id: string) => void;
-  init: () => void;
+  deInit: () => void;
+  signOut: () => void;
 }
+
+const initialState: ManagerModelState = {
+  initialized: false,
+  pk: null,
+  id: "",
+  name: "",
+  email: "",
+  phone: "",
+  job: "",
+  token: null,
+  refreshToken: null,
+};
 
 export const managerModel = create<ManagerModelState & ManagerModelAction>()(
   persist(
     (set, get) => ({
-      initialized: false,
-      id: "",
-      email: "",
-      phone: "",
-      token: null,
-      refreshToken: null,
+      ...initialState,
       setToken: (token, refreshToken) => {
         set({ token, refreshToken });
       },
-      setId: (id) => set({ id }),
-      init: () => {
-        return set({ initialized: true });
+      init: async () => {
+        if (!get().token) {
+          get().deInit();
+          return;
+        }
+
+        const res = await api.profile({});
+
+        if (isNil(res)) {
+          get().deInit();
+          return;
+        }
+
+        return set({
+          initialized: true,
+          pk: res.pk,
+          id: res.id,
+          name: res.name,
+          email: res.email,
+          phone: res.phone,
+          job: res.job,
+        });
       },
+      deInit: () => set({ ...initialState, initialized: true }),
+      signOut: () => get().deInit(),
     }),
     {
       name: config.sessionStorageKey,
