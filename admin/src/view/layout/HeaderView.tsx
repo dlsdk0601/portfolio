9c50a016398072c5447645f245a6e6ad5e993b20
dlsdk0601 <dlsdk0601@gmail.com>
2024-06-26 00:47:10 +0900

    feat: dropDownUserView 생성 후 HeaderView 에 추가

--- admin/src/view/layout/HeaderView.tsx
+++ admin/src/view/layout/HeaderView.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { DarkModeSwitcher, LeftSidebarButtonView } from "./headerButtonView";
+import DropDownUserView from "./DropDownUserView";
 
 export const HeaderView = () => {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <LeftSidebarButtonView />
-        <div className="flex items-center gap-3 2xsm:gap-7">
-          <ul className="flex items-center gap-2 2xsm:gap-4">
-            <DarkModeSwitcher />
-            {/* <DropdownUser /> */}
-          </ul>
-        </div>
+        <ul className="flex items-center gap-2 2xsm:gap-4">
+          <DarkModeSwitcher />
+        </ul>
+        <DropDownUserView />
       </div>
     </header>
   );
