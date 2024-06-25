d283521c5406a08ab7cf03a5f74fd42c7820d270
dlsdk0601 <dlsdk0601@gmail.com>
2024-06-26 00:45:13 +0900

    feat: profile API 생성

--- server/was/blueprints/sf/sign.py
+++ server/was/blueprints/sf/sign.py
@@ -22,7 +22,7 @@ class SignInRes(BaseModel):
 
 @router.post('/sign-in')
 def sign_in(req: SignInReq) -> Res[SignInRes]:
-    manager = db.sync_session.query(Manager).filter_by(id=req.id).one_or_none()
+    manager: Manager | None = db.sync_session.query(Manager).filter_by(id=req.id).one_or_none()
 
     if manager is None:
         return err('회원이 조회되지 않습니다.')
@@ -34,25 +34,6 @@ def sign_in(req: SignInReq) -> Res[SignInRes]:
     refresh_token = create_jwt_token(pk=manager.pk, expires_delta=timedelta(days=7))
 
     return ok(SignInRes(token=token, refresh_token=refresh_token))
-
-
-class AccessTokenReq(BaseModel):
-    pass
-
-
-class AccessTokenRes(BaseModel):
-    id: str
-    name: str
-
-
-@router.post('/access-token')
-def access_token(request: Request, _: AccessTokenReq) -> Res[AccessTokenRes]:
-    manager = db.sync_session.query(Manager).filter_by(pk=request.state.pk).one_or_none()
-
-    if not manager:
-        return err('회원이 조회되지 않습니다.\n다시 로그인을 해주세요.')
-
-    return ok(AccessTokenRes(id=manager.id, name=manager.name))
 
 
 class RefreshTokenReq(BaseModel):
@@ -67,3 +48,29 @@ class RefreshTokenRes(BaseModel):
 def refresh(request: Request, _: RefreshTokenReq) -> Res[RefreshTokenRes]:
     token = create_jwt_token(pk=request.state.pk)
     return ok(RefreshTokenRes(token=token))
+
+
+class ProfileReq(BaseModel):
+    pass
+
+
+class ProfileRes(BaseModel):
+    pk: int
+    id: str
+    name: str
+    email: str
+    phone: str
+    job: str
+
+
+@router.post('/profile')
+def profile(request: Request, _: ProfileReq) -> Res[ProfileRes]:
+    manager = db.sync_session.query(Manager).filter_by(pk=request.state.pk).one_or_none()
+
+    if not manager:
+        return err('회원이 조회되지 않습니다.\n다시 로그인을 해주세요.')
+
+    return ok(ProfileRes(
+        pk=manager.pk, id=manager.id, name=manager.name,
+        email=manager.email, phone=manager.phone, job=manager.job
+    ))
