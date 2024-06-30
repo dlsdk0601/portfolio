68c1abf24993fe0d511655c17d0cc1cb2315dee3
dlsdk0601 <dlsdk0601@gmail.com>
2024-07-01 00:03:39 +0900

    feat: manager list API 생성

--- server/was/blueprints/sf/manager.py
+++ server/was/blueprints/sf/manager.py
@@ -1,9 +1,67 @@
 from fastapi import APIRouter
+from starlette.requests import Request
 
-router = APIRouter()
+from ex.api import Res, ok, no_permission, BaseModel
+from ex.sqlalchemy_ex import isearch, Conditions, api_paginate, Pagination
+from was.model import db
+from was.model.manager import Manager, ManagerType
 
-# @router.get('/manager')
-# def manager():
-#     managers = db.session.query(Manager).all()
-#     print(managers)
-#     return 'hello world'
+router = APIRouter(prefix='/manager')
+
+
+class ManagerListReq(BaseModel):
+    page: int
+    search: str
+    enable: bool | None
+
+
+class ManagerListResItem(BaseModel):
+    pk: int
+    id: str
+    name: str
+    enable: bool
+
+    @classmethod
+    def from_model(cls, manager: Manager) -> 'ManagerListResItem':
+        return ManagerListResItem(
+            pk=manager.pk, id=manager.id, name=manager.name, enable=manager.enable
+        )
+
+
+class ManagerListRes(BaseModel):
+    pagination: Pagination[ManagerListResItem]
+
+
+@router.post('/manager-list')
+def manager_list(request: Request, req: ManagerListReq) -> Res[ManagerListRes]:
+    bg: Manager | None = request.state.manager
+
+    if bg is None or bg.type != ManagerType.SUPER:
+        return no_permission(None)
+
+    conditions: Conditions = [
+        Manager.pk != bg.pk,  # 본인 꼐정 조회는 제외
+        isearch(req.search, Manager.id, Manager.email, Manager.name),
+    ]
+
+    if req.enable is not None:
+        conditions.append(Manager.enable == req.enable)
+
+    q = db.sync_session.query(Manager).filter(*conditions).order_by(Manager.pk.desc())
+    pagination = api_paginate(query=q, page=req.page, map_=ManagerListResItem.from_model)
+
+    return ok(ManagerListRes(pagination=pagination))
+
+
+class ShowReq(BaseModel):
+    pk: int
+
+
+class ShowRes(BaseModel):
+    pk: int
+    id: str
+    name: str
+    email: str
+    phone: str
+    job: str
+    enable: bool
