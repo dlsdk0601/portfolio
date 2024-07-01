0700edfc28bcd0326fc1a7f6e52a0bc6c4aa268e
dlsdk0601 <dlsdk0601@gmail.com>
2024-07-02 02:03:57 +0900

    feat: string 타입을 각 타입에 맞춰서 처리 추가

---
+++ admin/src/ex/query.ts
@@ -0,0 +1,34 @@
+import moment from "moment/moment";
+import { isNil } from "lodash";
+
+const parseValue = (key: string, value: string) => {
+  if (key.endsWith("At")) {
+    return moment(value).toDate();
+  }
+
+  if (value === "true" || value === "false") {
+    return value === "true";
+  }
+
+  if (!isNaN(parseInt(value, 10))) {
+    return parseInt(value, 10);
+  }
+
+  return value;
+};
+
+type QUERY_VALUE = string | number | boolean | Date;
+
+export const parseQuery = <T extends Record<string, QUERY_VALUE>>(q: Partial<T>) => {
+  const query: Partial<T> = {};
+
+  Object.entries(q).forEach(([key, v]) => {
+    if (isNil(v)) {
+      return;
+    }
+
+    query[key as keyof T] = parseValue(key, v) as T[keyof T];
+  });
+
+  return query;
+};
