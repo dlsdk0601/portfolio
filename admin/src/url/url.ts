209de56e675796d0d9f50cc9e4053a1591e6285a
dlsdk0601 <dlsdk0601@gmail.com>
2024-07-05 01:57:58 +0900

    fix: path value 안바뀌는 이슈 수정

--- admin/src/url/url.ts
+++ admin/src/url/url.ts
@@ -1,5 +1,6 @@
 import _, { isNil } from "lodash";
 import { config } from "../config/config";
+import { isBlank } from "../ex/utils";
 
 export class PageUrl<T extends Record<string, any>> {
   readonly pathname: string;
@@ -13,8 +14,10 @@ export class PageUrl<T extends Record<string, any>> {
   }
 
   url(query?: T) {
+    let path = this.pathname;
+
     if (_.isNil(query)) {
-      return this.pathname;
+      return path;
     }
 
     const q: Record<string, string> = {};
@@ -25,15 +28,20 @@ export class PageUrl<T extends Record<string, any>> {
       }
 
       // query 값 중에 path에 있다면 거기에 처리 한다.
-      if (this.pathname.includes(item)) {
-        this.pathname.replace(`[${item}]`, `${query[item]}`);
+      if (path.includes(item)) {
+        path = this.pathname.replace(`[${item}]`, `${query[item]}`);
         return;
       }
 
       q[item] = `${query[item]}`;
     });
     const queryParams = new URLSearchParams(q);
-    return `${this.pathname}?${queryParams.toString()}`;
+
+    if (isBlank(queryParams.toString())) {
+      return path;
+    }
+
+    return `${path}?${queryParams.toString()}`;
   }
 
   urlPk(query: { pk: number | "new" }) {
