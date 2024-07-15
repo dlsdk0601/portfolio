a30fb2210882f786613a4d59a1972ab465e0ffd3
dlsdk0601 <dlsdk0601@gmail.com>
2024-07-15 22:36:09 +0900

    feat: CORSMiddleware 에 allow_origins 에 명시적으로 추가

--- server/was/application.py
+++ server/was/application.py
@@ -60,7 +60,7 @@ async def before_request(request: Request, call_next):
 if app.debug:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["http://localhost:3000"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
