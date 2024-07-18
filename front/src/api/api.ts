d4c5953c052a982a304abeaa470e72433714fb09
dlsdk0601 <dlsdk0601@gmail.com>
2024-07-19 01:23:13 +0900

    feat: error 처리에 대해 throw error 로 처리

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,6 +1,6 @@
-import { isString, map } from "lodash";
+import { map } from "lodash";
+import { notFound } from "next/navigation";
 import { ResStatus, ValidationError } from "./schema.g";
-import { Urls } from "../url/url.g";
 import { Api } from "./api.g";
 import { blockModel } from "../store/blockModel";
 
@@ -18,78 +18,46 @@ export interface ApiHandler {
 
 class Handler implements ApiHandler {
   handleStatus(status: ResStatus) {
-    if (typeof window === "undefined") {
-      alert("웹사이트 로딩중");
-    }
-
     switch (status) {
       case ResStatus.OK:
         return;
       case ResStatus.INVALID_ACCESS_TOKEN: {
-        // 처음 부터 로그인이 안된 경우, public 은 해당 사항이 없다.
-        // if (managerModel.getState().token === null) {
-        //   // return-to 체크가 불가능
-        //   window.location.href = Urls["sign-in"].page.url();
-        // }
-        //
-        // if (confirm("사용자 정보가 만료되었습니다.\n재시작 하시겠습니까?")) {
-        //   window.location.replace(Urls["sign-in"].page.url());
-        // }
-        return;
+        throw new Error("사용자 정보가 만료되었습니다.\n재시작 하시겠습니까?", {
+          cause: ResStatus.INVALID_ACCESS_TOKEN,
+        });
       }
       case ResStatus.LOGIN_REQUIRED: {
-        // public 은 해당 사항이 없다.
-        // alert("로그인 페이지로 이동합니다.");
-        // window.location.href = Urls["sign-in"].page.url();
-        return;
+        throw new Error("로그인 페이지로 이동합니다.", { cause: ResStatus.LOGIN_REQUIRED });
       }
       case ResStatus.NO_PERMISSION: {
-        alert("권한이 없습니다.");
-        window.location.href = Urls.page.url();
-        return;
+        throw new Error("권한이 없습니다.", { cause: ResStatus.NO_PERMISSION });
       }
       case ResStatus.NOT_FOUND:
       default: {
-        alert("존재하지 않는 페이지 또는 데이터입니다.");
+        notFound();
       }
     }
   }
 
   catch(e: any) {
+    // api 에서 catch 가 걸린거라면 알수 없는 에러이다.
     console.error(e);
-
-    if (e.message) {
-      alert(e.message);
-      return;
-    }
-
-    if (isString(e)) {
-      alert(e);
-      return;
-    }
-
-    alert(JSON.stringify(e));
+    throw new Error("예상치 못한 에러가 발생했습니다.");
   }
 
   handleValidationErrors(errors: ValidationError[]) {
     console.error(...errors);
-
-    try {
-      const messages = map(errors, (err) => `${err.loc} : ${err.msg}`).join("\n");
-      if (messages) {
-        alert(messages);
-      } else {
-        alert(JSON.stringify(errors, null, 2));
-      }
-    } catch (e) {
-      // pydanticValidationError 의 타입이 정확하지 않기 때문에 항상 출력
-      console.error(e);
-      alert(`${e}`);
+    const messages = map(errors, (err) => `${err.loc} : ${err.msg}`).join("\n");
+    if (messages) {
+      throw new Error(messages);
+    } else {
+      throw new Error(JSON.stringify(errors, null, 2));
     }
   }
 
   handlerErrors(errors: string[]) {
-    alert(errors.join("\n"));
+    console.error(...errors);
+    throw new Error(...errors);
   }
 
   with<T>(block: () => Promise<T>): Promise<T> {
---
+++ front/src/app/error.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ResStatus } from "../api/schema.g";
+import { Urls } from "../url/url.g";
+
+const ErrorPage = (props: { error: Error; reset: () => void }) => {
+  console.error(props.error);
+  const router = useRouter();
+
+  let onClick = () => props.reset();
+
+  switch (props.error.cause) {
+    case ResStatus.LOGIN_REQUIRED:
+    case ResStatus.NO_PERMISSION:
+    case ResStatus.INVALID_ACCESS_TOKEN: {
+      // 사실 로그인 페이지로 보내는게 맞지만, public 사이트는 불필요
+      onClick = () => router.push(Urls.page.url());
+      break;
+    }
+    default: {
+      onClick = () => props.reset();
+      break;
+    }
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+      <span className="mb-8 text-center text-xl text-zinc-400 duration-1000 group-hover:text-zinc-200">
+        {props.error.message ?? "예상치 못한 에러가 발생했습니다."}
+      </span>
+      <button
+        type="button"
+        className="rounded-2xl border-2 border-zinc-400 px-8 py-4 text-zinc-400 duration-700 hover:border-zinc-200 hover:text-zinc-200"
+        onClick={() => onClick()}
+      >
+        새로 고침
+      </button>
+    </div>
+  );
+};
+
+export default ErrorPage;
