e4d701cd14d202afde8d1090bd77123f844fa38b
dlsdk0601 <dlsdk0601@gmail.com>
2024-07-25 01:21:14 +0900

    refactor: generate 파일은 일단 주석 처리

--- new-server/bin/generate_ts_api.py
+++ new-server/bin/generate_ts_api.py
@@ -1,72 +1,72 @@
-import os
-import sys
-
-from more_itertools import flatten
-from stringcase import camelcase, capitalcase
-
-from ex.py.datetime_ex import now
-from was.application import app
-
-
-def main(prefix: str) -> None:
-    print('/* tslint:disable */')
-    print('/* eslint-disable */')
-    print(f'// 자동생성 파일 수정 금지 - {os.path.basename(__file__)} {now()}')
-    print('')
-
-    schemas = app.openapi()
-    # Base Class import
-    print('import { ApiBase } from "./apiBase"')
-    print('')
-    paths = list(filter(lambda x: x[0].startswith(prefix), schemas['paths'].items()))
-    schema_names = set(flatten([[f'{pascal_case(key)}Req', f'{pascal_case(key)}Res'] for key, _ in paths]))
-    print(f"import {'{' + ','.join(schema_names) + '}'} from './schema.g';")
-    print('')
-
-    # class define
-    print(f'export class Api extends ApiBase {{')
-    url: str
-    for url, value in paths:
-        url_name = pascal_case(url)
-
-        if 'get' in value:
-            print(
-                f"\treadonly {camelcase(url_name)} = "
-                f"this.g<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
-            )
-
-        if 'post' in value:
-            print(
-                f"\treadonly {camelcase(url_name)} = "
-                f"this.p<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
-            )
-
-        if 'put' in value:
-            print(
-                f"\treadonly {camelcase(url_name)} = "
-                f"this.pu<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
-            )
-
-        if 'delete' in value:
-            print(
-                f"\treadonly {camelcase(url_name)} = "
-                f"this.d<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
-            )
-    print('}')
-
-
-def pascal_case(name: str):
-    last_name = name.split('/')[-1]
-    url_name_split = list(map(lambda x: capitalcase(x), last_name.split('-')))
-    return ''.join(url_name_split)
-
-
-if __name__ == '__main__':
-    which = sys.argv[1:][0]
-    match which:
-        case 'app':
-            main('/app')
-        case 'front':
-            main('/front')
-        case default:
-            main('/sf')
+# import os
+# import sys
+#
+# from more_itertools import flatten
+# from stringcase import camelcase, capitalcase
+#
+# from ex.py.datetime_ex import now
+# from was.application import application
+#
+#
+# def main(prefix: str) -> None:
+#     print('/* tslint:disable */')
+#     print('/* eslint-disable */')
+#     print(f'// 자동생성 파일 수정 금지 - {os.path.basename(__file__)} {now()}')
+#     print('')
+#
+#     schemas = application.openapi()
+#     # Base Class import
+#     print('import { ApiBase } from "./apiBase"')
+#     print('')
+#     paths = list(filter(lambda x: x[0].startswith(prefix), schemas['paths'].items()))
+#     schema_names = set(flatten([[f'{pascal_case(key)}Req', f'{pascal_case(key)}Res'] for key, _ in paths]))
+#     print(f"import {'{' + ','.join(schema_names) + '}'} from './schema.g';")
+#     print('')
+#
+#     # class define
+#     print(f'export class Api extends ApiBase {{')
+#     url: str
+#     for url, value in paths:
+#         url_name = pascal_case(url)
+#
+#         if 'get' in value:
+#             print(
+#                 f"\treadonly {camelcase(url_name)} = "
+#                 f"this.g<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
+#             )
+#
+#         if 'post' in value:
+#             print(
+#                 f"\treadonly {camelcase(url_name)} = "
+#                 f"this.p<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
+#             )
+#
+#         if 'put' in value:
+#             print(
+#                 f"\treadonly {camelcase(url_name)} = "
+#                 f"this.pu<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
+#             )
+#
+#         if 'delete' in value:
+#             print(
+#                 f"\treadonly {camelcase(url_name)} = "
+#                 f"this.d<{capitalcase(url_name)}Req, {capitalcase(url_name)}Res>('{url}');"
+#             )
+#     print('}')
+#
+#
+# def pascal_case(name: str):
+#     last_name = name.split('/')[-1]
+#     url_name_split = list(map(lambda x: capitalcase(x), last_name.split('-')))
+#     return ''.join(url_name_split)
+#
+#
+# if __name__ == '__main__':
+#     which = sys.argv[1:][0]
+#     match which:
+#         case 'application':
+#             main('/application')
+#         case 'front':
+#             main('/front')
+#         case default:
+#             main('/sf')
