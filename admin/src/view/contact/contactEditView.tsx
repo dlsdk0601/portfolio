b8bc079a3107b406cdbb66d0720775b5f4e8e89e
inajung <ina.jung@coint.co.kr>
2024-08-05 16:17:58 +0900

    feat: onDelete API, onSubmit API 연동

--- admin/src/view/contact/contactEditView.tsx
+++ admin/src/view/contact/contactEditView.tsx
@@ -10,7 +10,7 @@ import { ContactShowRes, ContactType, contactTypeValues, toContactType } from ".
 import { ignorePromise, preventDefaulted } from "../../ex/utils";
 import { api } from "../../api/api";
 import useValueField from "../../hooks/useValueField";
-import { vRequired } from "../../ex/validate";
+import { validateFields, vRequired } from "../../ex/validate";
 import TextFieldView from "../textFieldView";
 import { ProfileIcon } from "../icons";
 import { SelectView } from "../selectView";
@@ -62,12 +62,55 @@ export const ContactFormEditView = (props: { contact: ContactShowRes | null }) =
       return;
     }
 
+    setType.set(contact.type);
     setId.set(contact.id);
     setHref.set(contact.href);
   }, [contact]);
 
+  const onSubmit = useCallback(async () => {
+    const isValid = validateFields([setId, setType, setHref]);
+
+    if (!isValid || isNil(type.value)) {
+      alert("데이터가 유효하지 않습니다.");
+      return;
+    }
+
+    const res = await api.contactEdit({
+      pk: isNil(contact) ? null : contact.pk,
+      type: type.value,
+      id: id.value,
+      href: href.value,
+    });
+
+    if (isNil(res)) {
+      return;
+    }
+
+    alert("저장되었습니다.");
+    router.replace(Urls.contact["[pk]"].page.url({ pk: res.pk }));
+  }, [contact, type, id, href]);
+
+  const onDelete = useCallback(async () => {
+    if (isNil(contact)) {
+      return;
+    }
+
+    if (!confirm("삭제 하시겠습니까?")) {
+      return;
+    }
+
+    const res = await api.deleteContact({ pk: contact.pk });
+
+    if (isNil(res)) {
+      return;
+    }
+
+    alert("삭제 되었습니다.");
+    router.replace(Urls.contact.page.url({}));
+  }, [contact]);
+
   return (
-    <form onSubmit={preventDefaulted(() => {})}>
+    <form onSubmit={preventDefaulted(() => onSubmit())}>
       <SelectView<ContactType | null>
         value={type.value}
         options={["타입", ...contactTypeValues].map((item) => ({
@@ -80,6 +123,13 @@ export const ContactFormEditView = (props: { contact: ContactShowRes | null }) =
       <TextFieldView field={href} onChange={(value) => setHref.set(value)} />
 
       <div className="flex justify-end gap-4.5">
+        <button
+          type="button"
+          className="mr-3 flex justify-center rounded bg-danger px-6 py-2 font-medium text-gray hover:bg-opacity-90"
+          onClick={() => onDelete()}
+        >
+          Delete
+        </button>
         <button
           type="submit"
           className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
