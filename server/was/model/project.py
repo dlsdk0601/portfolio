0779fa7ad5bd8335894c68709fed9fa0f23a5553
inajung <ina.jung@coint.co.kr>
2024-08-09 15:53:52 +0900

    feat: project 테이블 모델링 생성

---
+++ server/was/model/project.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from enum import auto
+
+from sqlalchemy import String, Text, DateTime, func
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ex.py.enum_ex import StringEnum
+from was.model import Model
+
+
+class ProjectType(StringEnum):
+    COMPANY = auto()
+    PERSONAL = auto()
+
+
+class Project(Model):
+    pk: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    type: Mapped[ProjectType] = mapped_column(postgresql.ENUM(ProjectType), nullable=False, comment='타입')
+    title: Mapped[str] = mapped_column(String(128), nullable=False, comment='프로젝트 타이틀')
+    description: Mapped[str] = mapped_column(Text, nullable=False, comment='프로젝트 간략 설명')
+
+    website_url: Mapped[str] = mapped_column(String(128), nullable=False, comment='웹사이트 링크')
+    github_url: Mapped[str] = mapped_column(String(128), nullable=False, comment='프로젝트 깃허브 링크')
+    main_text: Mapped[str] = mapped_column(Text, nullable=False, comment='프로젝트 주요 설명 MD 형식')
+
+    create_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    update_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    delete_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+    __table_args__ = (
+        {'comment': '프로젝트'}
+    )
