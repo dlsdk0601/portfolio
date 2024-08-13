e96e6136c1028396869776bcc1c648213130d57e
dlsdk0601 <dlsdk0601@gmail.com>
2024-08-13 23:51:18 +0900

    refactor: 불필요한 리렌더링을 막기 위해 버튼 컴포넌트화

--- admin/src/view/account/accountEditView.tsx
+++ admin/src/view/account/accountEditView.tsx
@@ -147,14 +147,20 @@ export const AccountFormEditView = (props: {
 
       <TextFieldView field={job} onChange={(value) => setJob.set(value)} />
 
-      <div className="flex justify-end gap-4.5">
-        <button
-          type="submit"
-          className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
-        >
-          Save
-        </button>
-      </div>
+      <AccountEditButtonView />
     </form>
+  );
+};
+
+const AccountEditButtonView = () => {
+  return (
+    <div className="flex justify-end gap-4.5">
+      <button
+        type="submit"
+        className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
+      >
+        Save
+      </button>
+    </div>
   );
 };
