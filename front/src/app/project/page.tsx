9e1e9d2c695461b104442973a6e25715bd4d6edb
inajung <ina.jung@coint.co.kr>
2024-08-16 14:58:56 +0900

    feat: project CardArticleView 생성 및 반영

--- front/src/app/project/page.tsx
+++ front/src/app/project/page.tsx
@@ -1,5 +1,52 @@
-const ProjectListPage = () => {
-  return <div>project list</div>;
+import { head, slice } from "lodash";
+import { CardArticleView } from "../../view/project/articleView";
+import { projectList } from "../../action/project";
+import { isNotNil } from "../../ex/utils";
+
+const ProjectListPage = async () => {
+  const projects = await projectList();
+  const headProject = head(projects);
+  const topProjects = slice(projects, 1, 3);
+  const subProjects = slice(projects, 3, projects.length);
+
+  return (
+    <>
+      <div className="mx-auto grid grid-cols-1 gap-8 lg:grid-cols-2">
+        {isNotNil(headProject) && <CardArticleView project={headProject} />}
+
+        <div className="mx-auto flex w-full flex-col gap-8 border-t border-gray-900/10 lg:mx-0 lg:border-t-0">
+          {topProjects.map((p) => (
+            <CardArticleView key={`project-list-${p.pk}`} project={p} />
+          ))}
+        </div>
+      </div>
+      <div className="hidden h-px w-full bg-zinc-800 md:block" />
+
+      <div className="mx-auto grid grid-cols-1 gap-4 md:grid-cols-3 lg:mx-0">
+        <div className="grid grid-cols-1 gap-4">
+          {subProjects
+            .filter((_, i) => i % 3 === 0)
+            .map((p) => (
+              <CardArticleView key={`project-list-${p.pk}`} project={p} />
+            ))}
+        </div>
+        <div className="grid grid-cols-1 gap-4">
+          {subProjects
+            .filter((_, i) => i % 3 === 1)
+            .map((p) => (
+              <CardArticleView key={`project-list-${p.pk}`} project={p} />
+            ))}
+        </div>
+        <div className="grid grid-cols-1 gap-4">
+          {subProjects
+            .filter((_, i) => i % 3 === 2)
+            .map((p) => (
+              <CardArticleView key={`project-list-${p.pk}`} project={p} />
+            ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectListPage;
---
+++ front/src/view/project/articleView.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { MouseEvent, PropsWithChildren } from "react";
+import { motion, useMotionTemplate, useSpring } from "framer-motion";
+import { isNil } from "lodash";
+import Link from "next/link";
+import { ProjectListResItem } from "../../api/schema.g";
+import { Urls } from "../../url/url.g";
+import { d2 } from "../../ex/dateEx";
+
+export const DefaultCardView = (props: PropsWithChildren) => {
+  const mouseX = useSpring(0, { stiffness: 500, damping: 100 });
+  const mouseY = useSpring(0, { stiffness: 500, damping: 100 });
+  const maskImage = useMotionTemplate`radial-gradient(240px at ${mouseX}px ${mouseY}px, white, transparent`;
+  const style = { maskImage, WebkitMaskImage: maskImage };
+
+  const onMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (isNil(e.currentTarget)) {
+      return;
+    }
+
+    const { left, top } = e.currentTarget.getBoundingClientRect();
+    mouseX.set(e.clientX - left);
+    mouseY.set(e.clientY - top);
+  };
+
+  return (
+    <div
+      onMouseMove={onMouseMove}
+      className="group relative overflow-hidden rounded-xl border border-zinc-600 duration-700 hover:border-zinc-400/50 hover:bg-zinc-800/10 md:gap-8"
+    >
+      <div className="pointer-events-none">
+        <div className="absolute inset-0 z-0 transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
+        <motion.div
+          className="absolute inset-0 z-10 bg-gradient-to-br via-zinc-100/10 opacity-100 transition duration-1000 group-hover:opacity-50"
+          style={style}
+        />
+        <motion.div
+          className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100"
+          style={style}
+        />
+      </div>
+
+      {props.children}
+    </div>
+  );
+};
+
+export const ArticleView = (props: { project: ProjectListResItem }) => {
+  return (
+    <Link href={Urls.project["[pk]"].page.url({ pk: props.project.pk })}>
+      <article className="p-4 md:p-8">
+        <div className="flex items-center justify-between gap-2">
+          <span className="drop-shadow-orange text-xs text-zinc-200 duration-1000 group-hover:border-zinc-200 group-hover:text-white">
+            <time>{d2(props.project.issueAt)}</time>
+          </span>
+        </div>
+        <h2 className="z-20 font-display text-xl font-medium text-zinc-200 duration-1000 group-hover:text-white lg:text-3xl">
+          {props.project.title}
+        </h2>
+        <p className="z-20 mt-4 text-sm text-zinc-400 duration-1000 group-hover:text-zinc-200">
+          {props.project.description}
+        </p>
+      </article>
+    </Link>
+  );
+};
+
+export const CardArticleView = (props: { project: ProjectListResItem }) => {
+  return (
+    <DefaultCardView>
+      <ArticleView project={props.project} />
+    </DefaultCardView>
+  );
+};
