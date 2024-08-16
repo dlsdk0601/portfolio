feb483d6b4dd179bd3f68884dd83b6db6c0992e5
inajung <ina.jung@coint.co.kr>
2024-08-16 14:58:34 +0900

    feat: project list server action 추가

---
+++ front/src/action/project.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { isNil } from "lodash";
+import { notFound } from "next/navigation";
+import { api } from "../api/api";
+
+export async function projectList() {
+  const res = await api.projectList({});
+
+  if (isNil(res)) {
+    notFound();
+  }
+
+  return res.projects;
+}
