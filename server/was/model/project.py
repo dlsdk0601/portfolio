eefe486e57ffee01a9915669d05828ef64dedaad
dlsdk0601 <dlsdk0601@gmail.com>
2024-08-19 23:39:03 +0900

    feat: view count property 추가

--- server/was/model/project.py
+++ server/was/model/project.py
@@ -1,12 +1,12 @@
 from datetime import datetime, date
 from enum import auto
 
-from sqlalchemy import String, Text, DateTime, func, Date, ForeignKey
+from sqlalchemy import String, Text, DateTime, func, Date, ForeignKey, select
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ex.py.enum_ex import StringEnum
-from was.model import Model
+from was.model import Model, db
 
 
 class ProjectType(StringEnum):
@@ -25,10 +25,22 @@ class Project(Model):
     github_url: Mapped[str] = mapped_column(String(128), nullable=False, comment='프로젝트 깃허브 링크')
     main_text: Mapped[str] = mapped_column(Text, nullable=False, comment='프로젝트 주요 설명 MD 형식')
     issue_at: Mapped[date] = mapped_column(Date(), nullable=False, comment='배포 일자')
+    view_logs: Mapped[list['ProjectViewLog']] = relationship(back_populates='project', uselist=True)
 
     create_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     update_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
     delete_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+    @property
+    def view_count(self) -> int:
+        view_log_count = (db
+                          .session
+                          .execute(
+            select(func.count(ProjectViewLog.pk)).where(ProjectViewLog.project_pk == self.pk)
+        )
+                          .scalar_one())
+
+        return view_log_count
 
     __table_args__ = (
         {'comment': '프로젝트'}
@@ -39,7 +51,7 @@ class ProjectViewLog(Model):
     pk: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
     project_pk: Mapped[int] = mapped_column(ForeignKey(Project.pk), nullable=False, comment='프로젝트 - FK')
-    project: Mapped[Project] = relationship()
+    project: Mapped[Project] = relationship(back_populates='view_logs')
     create_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
     __table_args__ = (
