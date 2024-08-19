d4ef7790121b2619eaa1b0db2dd0f0d6e464a5c3
dlsdk0601 <dlsdk0601@gmail.com>
2024-08-19 22:39:23 +0900

    feat: projectShow API 연동 및 HeaderView 반영

--- front/src/app/project/[pk]/page.tsx
+++ front/src/app/project/[pk]/page.tsx
@@ -1,9 +1,39 @@
+import { isNil } from "lodash";
+import { notFound } from "next/navigation";
+import { SearchParams } from "../../../type/definitions";
+import { parseQuery } from "../../../ex/query";
+import { projectShow } from "../../../action/project";
+import ProjectShowHeaderView from "../../../view/project/projectShowHeaderView";
+
 export interface Query {
   pk: number;
 }
 
-const Page = () => {
-  return <div>test</div>;
+const Page = async (props: { params: SearchParams }) => {
+  const param = parseQuery<Query>(props.params);
+
+  if (isNil(param.pk)) {
+    return notFound();
+  }
+
+  const project = await projectShow(param.pk);
+
+  if (isNil(project)) {
+    return <></>;
+  }
+
+  return (
+    <div className="min-h-screen bg-zinc-50">
+      <ProjectShowHeaderView
+        title={project.title}
+        description={project.description}
+        githubSite={project.githubUrl}
+        websiteUrl={project.websiteUrl}
+        views={100}
+      />
+      <article className="prose prose-zinc prose-quoteless mx-auto px-4 py-12">test</article>
+    </div>
+  );
 };
 
 export default Page;
