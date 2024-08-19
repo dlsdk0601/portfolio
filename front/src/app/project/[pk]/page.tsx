d9d6e5da3334f4f8e6db6d73186ec144f34c94ee
dlsdk0601 <dlsdk0601@gmail.com>
2024-08-19 23:41:10 +0900

    feat: action 함수 포함하는 컴포넌트 분리 시키고 suspense 추가

--- front/src/app/project/[pk]/page.tsx
+++ front/src/app/project/[pk]/page.tsx
@@ -1,9 +1,12 @@
 import { isNil } from "lodash";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 import { SearchParams } from "../../../type/definitions";
 import { parseQuery } from "../../../ex/query";
-import { projectShow } from "../../../action/project";
+import { projectShow, projectView } from "../../../action/project";
 import ProjectShowHeaderView from "../../../view/project/projectShowHeaderView";
+import MdEditorViewer from "../../../view/project/MDEditorViewer";
+import ProjectShowSkeletonView from "../../../view/skeleton/projectShowSkeletonView";
 
 export interface Query {
   pk: number;
@@ -16,23 +19,36 @@ const Page = async (props: { params: SearchParams }) => {
     return notFound();
   }
 
-  const project = await projectShow(param.pk);
+  return (
+    <div className="min-h-screen bg-zinc-50">
+      <Suspense fallback={<ProjectShowSkeletonView />}>
+        <ProjectShowView pk={param.pk} />
+      </Suspense>
+    </div>
+  );
+};
 
-  if (isNil(project)) {
+const ProjectShowView = async (props: { pk: number }) => {
+  const views = await projectView(props.pk);
+  const project = await projectShow(props.pk);
+
+  if (isNil(project) || isNil(views)) {
     return <></>;
   }
 
   return (
-    <div className="min-h-screen bg-zinc-50">
+    <>
       <ProjectShowHeaderView
         title={project.title}
         description={project.description}
         githubSite={project.githubUrl}
         websiteUrl={project.websiteUrl}
-        views={100}
+        views={views}
       />
-      <article className="prose prose-zinc prose-quoteless mx-auto px-4 py-12">test</article>
-    </div>
+      <article className="prose prose-zinc prose-quoteless mx-auto px-4 py-12">
+        <MdEditorViewer mainText={project.mainText} />
+      </article>
+    </>
   );
 };
 
---
+++ front/src/view/skeleton/projectShowSkeletonView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectShowSkeletonView = () => {
+  return <div>test</div>;
+};
+
+export default ProjectShowSkeletonView;
