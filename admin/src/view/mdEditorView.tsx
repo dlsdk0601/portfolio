eff6c7e5cb8fa6d2b44422fa0d2cf544477c0b3b
dlsdk0601 <dlsdk0601@gmail.com>
2024-08-21 13:56:12 +0900

    feat: MD editor 에 label 및 error 처리 추가

--- admin/src/view/mdEditorView.tsx
+++ admin/src/view/mdEditorView.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import MDEditor, { commands } from "@uiw/react-md-editor";
 import { ExecuteState, TextAreaTextApi } from "@uiw/react-md-editor/src/commands";
 import { isNil } from "lodash";
+import classNames from "classnames";
 import { ValueField } from "../hooks/useValueField";
 import useDarkMode from "../hooks/useDarkMode";
 import { blockModel } from "../store/blockModel";
 import { onNewAsset } from "../ex/fileEx";
+import { isNotBlank } from "../ex/utils";
 
 const c = [
   commands.bold,
@@ -53,19 +55,36 @@ const c = [
   },
 ];
 
-const MdEditorView = (props: { field: ValueField<string>; onChange: (value: string) => void }) => {
+const MdEditorView = (props: {
+  field: ValueField<string>;
+  onChange: (value: string) => void;
+  label?: string;
+  height?: number;
+}) => {
   const { isDark } = useDarkMode();
 
   return (
-    <div className="markarea">
-      <div data-color-mode={isDark ? "dark" : "light"}>
-        <MDEditor
-          height={300}
-          value={props.field.value}
-          onChange={(value) => props.onChange(value ?? "")}
-          commands={c}
-        />
+    <div className="mb-5.5 w-full">
+      <label className="mb-2.5 block text-sm font-medium text-black dark:text-white">
+        {props.label ?? props.field.name}
+      </label>
+      <div
+        className={classNames("markarea", {
+          "border-meta-1 focus:border-meta-1": isNotBlank(props.field.error),
+        })}
+      >
+        <div data-color-mode={isDark ? "dark" : "light"}>
+          <MDEditor
+            height={props.height ?? 300}
+            value={props.field.value}
+            onChange={(value) => props.onChange(value ?? "")}
+            commands={c}
+          />
+        </div>
       </div>
+      {isNotBlank(props.field.error) && (
+        <p className="mt-1 text-xs italic text-meta-1">{props.field.error}</p>
+      )}
     </div>
   );
 };
