ec03fc86244b589804a4c5bfd8ef9641acb2715d
dlsdk0601 <dlsdk0601@gmail.com>
2024-08-28 01:24:27 +0900

    feat: TODO 제거 및 컬러 추가

--- portfolio_app/lib/color.dart
+++ portfolio_app/lib/color.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-// TODO :: 색상 정의
 const c000000 = Color(0xFF000000);
 const cFFFFFF = Color(0xFFFFFFFF);
+const c71717A = Color(0xFF71717A);
+const cD4D4D8 = Color(0xFFD4D4D8);
 
-const primaryColor = c000000;
+const primaryColor = c71717A;
 const backgroundColor = c000000;
