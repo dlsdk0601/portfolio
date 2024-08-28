2152ee5e767f9613c47ba9347413ba875d83e279
dlsdk0601 <dlsdk0601@gmail.com>
2024-08-28 23:52:01 +0900

    feat: router 정의 추가

--- portfolio_app/lib/router.dart
+++ portfolio_app/lib/router.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:portfolio_app/screen/contact/contacts.dart';
 import 'package:portfolio_app/screen/home.dart';
+import 'package:portfolio_app/screen/project/list.dart';
+import 'package:portfolio_app/screen/project/project.dart';
 
 part 'router.g.dart';
 
 @TypedGoRoute<HomeRoute>(path: '/', routes: [
   TypedGoRoute<ContactsRoute>(path: 'contacts'),
+  TypedGoRoute<ProjectListRoute>(path: 'project/list', routes: [
+    TypedGoRoute<ProjectShowRoute>(path: 'project/show/:pk'),
+  ])
 ])
 class HomeRoute extends GoRouteData {
   const HomeRoute();
@@ -21,6 +26,24 @@ class ContactsRoute extends GoRouteData {
   @override
   Widget build(BuildContext context, GoRouterState state) =>
       const ContactsScreen();
+}
+
+class ProjectListRoute extends GoRouteData {
+  const ProjectListRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) =>
+      const ProjectListScreen();
+}
+
+class ProjectShowRoute extends GoRouteData {
+  const ProjectShowRoute(this.pk);
+
+  final int pk;
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) =>
+      ProjectShowScreen(pk: pk);
 }
 
 const defaultLocation = '';
