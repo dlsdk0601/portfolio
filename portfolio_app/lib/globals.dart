1ee98d5dae5aab8d0db919d7ece115bf2b8a6058
inajung <ina.jung@coint.co.kr>
2024-08-30 12:34:02 +0900

    feat: api 인스턴스 추가

--- portfolio_app/lib/globals.dart
+++ portfolio_app/lib/globals.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:logger/logger.dart';
+import 'package:portfolio_app/api/api.gen.dart';
+import 'package:portfolio_app/config.dart';
 import 'package:uuid/data.dart';
 import 'package:uuid/rng.dart';
 import 'package:uuid/uuid.dart';
@@ -7,6 +9,9 @@ import 'package:uuid/uuid.dart';
 // 초기 실행 설정이 반영된 후 처리 되도록
 // ignore: unnecessary_late
 late final logger = Logger();
+
+// ignore: unnecessary_late
+late final api = Api(config.apiServer);
 
 final cacheManager = DefaultCacheManager();
 
