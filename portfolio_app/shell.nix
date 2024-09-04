598a2f283b750d3f06429782f431c4245ec956d8
inajung <ina.jung@coint.co.kr>
2024-09-04 19:18:59 +0900

    feat: app name 수정

--- portfolio_app/shell.nix
+++ portfolio_app/shell.nix
@@ -5,11 +5,11 @@ let
       "https://github.com/NixOS/nixpkgs/tarball/63d37ccd2d178d54e7fb691d7ec76000740ea24a";
     sha256 = "0ymvnql1sxij93q0f88dv5877mx32pldz77w72vmzayxwkrq5h7d";
   }) { };
-  xsailr-app = with pkgs; [ cocoapods ];
-  packages = xsailr-app;
+  portfolio_app = with pkgs; [ cocoapods ];
+  packages = portfolio_app;
 in { shellHook ? "" }:
 pkgs.mkShellNoCC {
-  name = "xailr-app";
+  name = "portfolio_app";
   inherit packages;
   inherit shellHook;
 }
