559a271f50aa6b38b1e069fc6aa6764be6cf1d51
dlsdk0601 <dlsdk0601@gmail.com>
2024-09-06 23:51:33 +0900

    feat: Layout 위젯으로 감싸기

--- portfolio_app/lib/screen/home.dart
+++ portfolio_app/lib/screen/home.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:portfolio_app/router.dart';
+import 'package:portfolio_app/view/layout.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const Text('홈'),
-          ElevatedButton(
-            onPressed: () => context.go(const ContactsRoute().location),
-            child: const Text("go contact"),
-          ),
-          const SizedBox(
-            height: 18.0,
-          ),
-          ElevatedButton(
-            onPressed: () => context.go(const ProjectListRoute().location),
-            child: const Text("go project"),
-          )
-        ],
+    return Layout(
+      title: 'HOME',
+      context: context,
+      child: Center(
+        child: Column(
+          children: [
+            const Text('홈'),
+            ElevatedButton(
+              onPressed: () => context.go(const ContactsRoute().location),
+              child: const Text("go contact"),
+            ),
+            const SizedBox(
+              height: 18.0,
+            ),
+            ElevatedButton(
+              onPressed: () => context.go(const ProjectListRoute().location),
+              child: const Text("go project"),
+            )
+          ],
+        ),
       ),
     );
   }
