6deb90bf8b70a91aa3e60ed267718c3775273ddd
inajung <ina.jung@coint.co.kr>
2024-09-13 15:31:48 +0900

    docs: 색깔 구분을 위해 주석 추가

--- portfolio_app/lib/color.dart
+++ portfolio_app/lib/color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const c000000 = Color(0xFF000000);
-const cFFFFFF = Color(0xFFFFFFFF);
-const c71717A = Color(0xFF71717A);
+const c000000 = Color(0xFF000000); // black
+const cFFFFFF = Color(0xFFFFFFFF); // white
+const c71717A = Color(0xFF71717A); // zinc
 const cD4D4D8 = Color(0xFFD4D4D8);
 
 const primaryColor = c71717A;
