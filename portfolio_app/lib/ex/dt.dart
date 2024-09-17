c7f00ed82d66900409bbe01ce0f8e6e0d57b387a
dlsdk0601 <dlsdk0601@gmail.com>
2024-09-17 18:29:27 +0900

    feat: project 에서 사용할 dateFormat 추가

--- portfolio_app/lib/ex/dt.dart
+++ portfolio_app/lib/ex/dt.dart
@@ -1,7 +1,9 @@
 import 'package:intl/intl.dart';
 
 extension DateTimeDtExtension on DateTime {
-  String get dt => DateFormat('yyyy.MM.dd').format(toLocal());
+  String get d1 => DateFormat('yyyy.MM.dd').format(toLocal());
+
+  String get d2 => DateFormat('MMM d, yyyy').format(toLocal());
 
   String get dt1 => DateFormat('yyyy.MM.dd HH:mm').format(toLocal());
 
