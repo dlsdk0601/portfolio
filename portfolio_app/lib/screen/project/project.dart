249aadd040bcb3b122d5a3fe8a02c963dede6ab4
dlsdk0601 <dlsdk0601@gmail.com>
2024-09-18 19:41:24 +0900

    feat: project 퍼블 및 flutter_markdown 반영

--- portfolio_app/lib/screen/project/project.dart
+++ portfolio_app/lib/screen/project/project.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portfolio_app/api/schema.gen.dart';
+import 'package:portfolio_app/color.dart';
+import 'package:portfolio_app/ex/app.dart';
 import 'package:portfolio_app/ex/dt.dart';
 import 'package:portfolio_app/view/layout.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -33,28 +36,59 @@ class ProjectShowScreen extends HookConsumerWidget {
 
     if (model.project == null) {
       return const Center(
-        child: Text('데이터가 조회되지 않습니다.'),
+        child: CircularProgressIndicator(),
       );
     }
 
     final project = model.project!;
 
     return Layout(
-      title: 'project title',
+      title: project.title,
       context: context,
-      child: SafeArea(
-        child: Column(
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(project.pk.toString()),
-            Text(project.title),
-            Text(project.type.toString()),
-            Text(project.description),
-            Text(project.websiteUrl),
-            Text(project.githubUrl),
-            Text(project.mainText),
-            Text(project.createAt.d2),
+            const Icon(
+              Icons.remove_red_eye_outlined,
+              size: 18.0,
+              color: primaryColor,
+            ),
+            const SizedBox(
+              width: 5.0,
+            ),
+            Text(
+              2.toString(),
+              style: const TextStyle(
+                fontSize: 16.0,
+                color: primaryColor,
+                height: 1.0,
+              ),
+            ),
+            const SizedBox(
+              width: 5.0,
+            ),
           ],
-        ),
+        )
+      ],
+      child: ListView(
+        children: [
+          _HeaderView(
+            project: project,
+          ),
+          Container(
+            color: cFFFFFF,
+            padding: const EdgeInsets.symmetric(
+              vertical: 48.0,
+              horizontal: 16.0,
+            ),
+            // height: 900.0,
+            child: MarkdownBody(
+              data: project.mainText,
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -88,4 +122,99 @@ class _Model with _$Model {
     required bool initialized,
     required ProjectShowRes? project,
   }) = __Model;
+}
+
+class _HeaderView extends StatelessWidget {
+  final ProjectShowRes project;
+
+  const _HeaderView({
+    super.key,
+    required this.project,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 15.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            project.title,
+            style: const TextStyle(
+              fontSize: 36.0,
+              color: cFFFFFF,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(
+            height: 24.0,
+          ),
+          Text(
+            project.description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 18.0,
+              color: cFFFFFF,
+            ),
+          ),
+          const SizedBox(
+            height: 40.0,
+          ),
+          if (project.githubUrl.isNotEmpty)
+            linkButtonView(
+              onTap: () async {
+                if (project.githubUrl.isEmpty) {
+                  return;
+                }
+                await openBrowser(project.githubUrl, context);
+              },
+              url: project.githubUrl,
+              title: "GitHub",
+            ),
+          const SizedBox(
+            height: 28.0,
+          ),
+          if (project.websiteUrl.isNotEmpty)
+            linkButtonView(
+              onTap: () async {
+                if (project.websiteUrl.isEmpty) {
+                  return;
+                }
+                await openBrowser(project.websiteUrl, context);
+              },
+              url: project.websiteUrl,
+              title: "WebSite",
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget linkButtonView({
+    required VoidCallback onTap,
+    required String url,
+    required String title,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 16.0, color: cFFFFFF),
+          ),
+          const SizedBox(
+            width: 5.0,
+          ),
+          const Icon(Icons.arrow_right_alt),
+        ],
+      ),
+    );
+  }
 }
